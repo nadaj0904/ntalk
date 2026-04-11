cd18ae9b794054ffb99c6ff076209f9e0ad01b3c
Namchang Lee <namchang@example.com>
2026-04-12 07:18:02 +0900

    secod

--- src/main/java/com/ntalk/choi/NtalkApplication.java
+++ src/main/java/com/ntalk/choi/NtalkApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("com.ntalk.choi.repository")
 public class NtalkApplication {
-    //start
+    //start!!
 	public static void main(String[] args) {
 		SpringApplication.run(NtalkApplication.class, args);
 	}
